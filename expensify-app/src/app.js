696e8d8d19924476afbf4fe739a43cc4d0572b67
AliNisarAhmed <ali.nisar87@gmail.com>
2018-08-04 17:40:31 +0500

    Higher Order Components

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -4,6 +4,34 @@ import AppRouter from "./routers/AppRouter";
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
+import configureStore from './store/configureStore';
+import { addExpense, removeExpense, editExpense } from './actions/expenses';
+import { setTextFilter, sortByDate, sortByAmount, setStartDate, setEndDate } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
+
+
+const store = configureStore();
+
+
+store.dispatch(addExpense({description: 'water bill'}));
+
+store.dispatch(addExpense({description: 'Gas bill'}));
+
+store.dispatch(setTextFilter('gas'));
+
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+
+console.log(visibleExpenses);
+
+console.log(store.getState());
+
+// store.subscribe(() => {
+//   const state = store.getState();
+//   const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+//   console.log(visibleExpenses);
+// });
+
 
 
 
