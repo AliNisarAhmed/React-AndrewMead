1979fda693abfabf3860d9eb107e59e072a163cb
AliNisarAhmed <ali.nisar87@gmail.com>
2018-08-19 11:23:17 +0500

    setting up enzyme 2

--- expensify-app/src/tests/components/Header.test.js
+++ expensify-app/src/tests/components/Header.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import ReactShallowRenderer from 'react-test-renderer/shallow';
+import { shallow } from 'enzyme';  // we have switched from react-test-renderer to enzyme
 import Header from '../../components/Header';
+// import toJSON from 'enzyme-to-json';  // Removed as the toJSON already incorporated in enzyme config file
 
 test('should render Header correctly', () => {
-  const renderer = new ReactShallowRenderer();
-  renderer.render(<Header />);
-  expect(renderer.getRenderOutput()).toMatchSnapshot();
+  const wrapper = shallow(<Header />);
+  expect(wrapper).toMatchSnapshot();
+
+
+  // expect(wrapper.find('h1').text()).toBe('Expensify');
+  
+  // const renderer = new ReactShallowRenderer();
+  // renderer.render(<Header />);
+  // expect(renderer.getRenderOutput()).toMatchSnapshot();
 });
