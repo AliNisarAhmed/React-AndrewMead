ca8569bc80e3da247767a92d82a79ad412310e92
AliNisarAhmed <ali.nisar87@gmail.com>
2018-08-30 23:00:04 +0500

    testing using spies - modifying dumb components to class and use of mapDispatchToProps

--- expensify-app/src/components/AddExpensePage.js
+++ expensify-app/src/components/AddExpensePage.js
@@ -3,16 +3,51 @@ import { connect } from "react-redux";
 import ExpenseForm from './ExpenseForm';
 import { addExpense } from '../actions/expenses';
 
+export class AddExpensePage extends React.Component {
+
+  onSubmit = (expense) => {
+    this.props.addExpense(expense);
+    this.props.history.push('/');
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Add Expense</h1>
+        <ExpenseForm 
+          onSubmit={this.onSubmit}
+        />
+      </div>
+    );
+  }
+}
+
+
+/*
+
 const AddExpensePage = (props) => (  // props given by the connect call below 
   <div>
     <h1>Add Expense</h1>
     <ExpenseForm 
       onSubmit={(expense) => {
-        props.dispatch(addExpense(expense));
+        //props.dispatch(addExpense(expense));
+        props.onSubmit(expense); 
+        // The above step helps us in converting the props.dispatch call to a more testable version, (addExpense function is the problem in testing since it is imported rather than passed down as props).
+
+        // We use mapDispatchToProps from redux api below.
+        
         props.history.push('./');  // this is the prop passed by React Routerm and is used to re route the page to the given path after submit.
       }}
     />
   </div>
 );
 
-export default connect()(AddExpensePage);
+*/
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addExpense: (expense) => dispatch(addExpense(expense))
+  };
+};
+
+export default connect(undefined, mapDispatchToProps)(AddExpensePage);
--- expensify-app/src/components/EditExpensePage.js
+++ expensify-app/src/components/EditExpensePage.js
@@ -3,6 +3,51 @@ import { connect } from 'react-redux';
 import ExpenseForm from './ExpenseForm';
 import { editExpense, removeExpense } from '../actions/expenses';
 
+export class EditExpensePage extends React.Component {
+  onSubmit = (expense) => {
+    this.props.editExpense(this.props.expense.id, expense);
+    this.props.history.push('/'); 
+  };
+
+  onClick = () => {
+    this.props.removeExpense(this.props.expense);
+    this.props.history.push('/');
+  };
+
+  render() {
+    return (
+      <div>
+        <ExpenseForm 
+          onSubmit={this.onSubmit}
+          expense={this.props.expense}
+        />
+        <button 
+          onClick={this.onClick}
+        >
+          Remove
+        </button>
+    </div>
+    );
+  }
+}
+
+const mapStateToProps = (state, props) => {
+  return {
+    expense: state.expenses.find(expense => expense.id === props.match.params.id)
+  };
+}; 
+
+const mapDispatchToProps = (dispatch, props) => {
+  return {
+    editExpense: (id, expense) => dispatch(editExpense(id, expense)),
+    removeExpense: (data) => dispatch(removeExpense(data))
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(EditExpensePage);
+
+
+/*
 const EditExpensePage = (props) => {
   return (
     <div>
@@ -28,10 +73,4 @@ const EditExpensePage = (props) => {
   );
 };
 
-const mapStateToProps = (state, props) => {
-  return {
-    expense: state.expenses.find(expense => expense.id === props.match.params.id)
-  }
-}; 
-
-export default connect(mapStateToProps)(EditExpensePage);
+*/
